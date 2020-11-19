88f2b54e3a213a155664412d1e4b994b46a98831
Christian <christiandp7@gmail.com>
2020-11-19 13:39:47 -0400

    ADD - archivo page layout ready, logoVerticalLeft ready!

--- src/Pages/Archivo.jsx
+++ src/Pages/Archivo.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import HomeSliderSlick from '../Components/Home/HomeSliderSlick'
+import { Block } from 'baseui/block'
+import { NavLink as Link } from 'react-router-dom'
+
+import { LogoVerticalLeft } from '../Svg/Logo'
 
 function Archivo() {
   return (
-    <div>
-      <h1>Archivo</h1>
-    </div>
+    <Block 
+      display="flex"
+      justifyContent="flex-end"
+      height="100vh"
+      /*display="flex"
+      justifyContent="flex-end"
+      flexWrap="wrap"*/
+    >
+      <Block width="15%" maxHeight="100vh" display="flex" alignItems="center" justifyContent="flex-start" >
+        <Link to="/">
+          <LogoVerticalLeft />
+        </Link>
+      </Block>
+      <Block width="85%" display="flex" justifyContent="flex-end" alignItems="center">
+        <Block display="block" maxWidth="660px" width="100%">
+          {/*<HomeSliderSlick />*/}
+          <HomeSliderSlick />
+        </Block>
+        
+      </Block>
+      
+      
+    </Block>
   )
 }
 
