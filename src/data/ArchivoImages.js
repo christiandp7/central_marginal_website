94f9aa1586f79894f217e7b0018b9ba88eeaba9e
Christian <christiandp7@gmail.com>
2020-12-21 20:41:20 -0400

    Working on Archivo, only left add some images

--- src/data/ArchivoImages.js
+++ src/data/ArchivoImages.js
@@ -6,10 +6,13 @@ import archivoImg5 from '../assets/images/archivo/archivo-05.jpg'
 import archivoImg6 from '../assets/images/archivo/archivo-06.jpg'
 import archivoImg7 from '../assets/images/archivo/archivo-07.jpg'
 import archivoImg8 from '../assets/images/archivo/archivo-08.jpg'
+import archivoImg9 from '../assets/images/archivo/archivo-09.jpg'
+import archivoImg10 from '../assets/images/archivo/archivo-10.jpg'
+import archivoImg11 from '../assets/images/archivo/archivo-11.jpg'
+import archivoImg12 from '../assets/images/archivo/archivo-12.jpg'
 // Home images
 import homeImg1 from '../assets/images/home/home-01.jpg'
 import homeImg4 from '../assets/images/home/home-04.jpg'
-import homeImg6 from '../assets/images/home/home-06.jpg'
 
 const halfCol = [16, 4, 6];
 const wholeCol = [16, 8, 12];
@@ -36,8 +39,28 @@ const ArchivoImages = [
     col: halfCol,
   },
   {
+    src: archivoImg9,
+    col: halfCol,
+  },
+  {
+    src: archivoImg10,
+    col: wholeCol,
+  },
+  {
+    src: archivoImg11,
+    col: halfCol,
+  },
+  {
     src: archivoImg3,
     col: halfCol,
+  },
+  {
+    src: homeImg1,
+    col: halfCol,
+  },
+  {
+    src: archivoImg12,
+    col: wholeCol,
   },
   {
     src: archivoImg4,
