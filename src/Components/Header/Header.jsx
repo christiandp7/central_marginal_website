31498fdcdda92b6d19a75de8e664deff4fe1bf8b
Christian Diaz Portela <christiandp7@gmail.com>
2021-01-13 14:25:54 +0100

    Fix update on mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { Block } from 'baseui/block'
-import {Grid, Cell} from 'baseui/layout-grid';
+import { Grid, Cell } from 'baseui/layout-grid';
 import { Link } from 'react-router-dom'
 import Fade from 'react-reveal/Fade';
-import {Button, SHAPE, KIND, SIZE} from 'baseui/button';
+import { Button, SHAPE, KIND, SIZE } from 'baseui/button';
 import IconMenu from "baseui/icon/menu";
 
 // components
@@ -12,50 +12,50 @@ import MobileNav from '../MobileNav/MobileNav'
 
 
 
-function Header({ noLogo }) {
+function Header({ noLogo, onlyMobile }) {
   const [navOpen, setNavOpen] = useState(false)
   return (
     <>
-    <Grid
-      gridMargins={0}
-    >
-      <Cell span={12}>
-        <Block display="flex" alignItems="center">
-          
-          <Button 
-            onClick={() => setNavOpen(true)} 
-            shape={SHAPE.circle} 
-            kind={KIND.tertiary} 
-            size={SIZE.large}
-            $style={({$theme}) => ({
-              marginLeft: "5px",
-              marginRight: "5px",
-              //"@media screen and (min-width: 1136px)": {
-              [$theme.mediaQuery.large]: {
-                display: "none"
-              }
-            })}
-          >
-            <IconMenu size={24} />
-          </Button>
-          
-          {
-            !noLogo && (
-              <div className="logo_container_hz">
-                <Link to="/">
-                  <Fade top>
-                    <LogoHorizontal />
-                  </Fade>
-                </Link>
-              </div>
-            )
-          }
-          
+      <Grid
+        gridMargins={0}
+      >
+        <Cell span={12}>
+          <Block display="flex" alignItems="center">
 
-        </Block>
-      </Cell>
-    </Grid>
-    <MobileNav isOpen={navOpen} setIsOpen={setNavOpen} />
+            <Button
+              onClick={() => setNavOpen(true)}
+              shape={SHAPE.circle}
+              kind={KIND.tertiary}
+              size={SIZE.large}
+              $style={({ $theme }) => ({
+                marginLeft: "5px",
+                marginRight: "5px",
+                //"@media screen and (min-width: 1136px)": {
+                [$theme.mediaQuery.large]: {
+                  display: "none"
+                }
+              })}
+            >
+              <IconMenu size={24} />
+            </Button>
+
+            {
+              !noLogo && (
+                <div className={`logo_container_hz ${onlyMobile && "onlyMobile"}`}>
+                  <Link to="/">
+                    <Fade top>
+                      <LogoHorizontal />
+                    </Fade>
+                  </Link>
+                </div>
+              )
+            }
+
+
+          </Block>
+        </Cell>
+      </Grid>
+      <MobileNav isOpen={navOpen} setIsOpen={setNavOpen} />
     </>
   )
 }
--- src/Pages/Home2.jsx
+++ src/Pages/Home2.jsx
@@ -3,6 +3,7 @@ import { Block } from 'baseui/block'
 //import { useStyletron } from 'baseui'
 import { NavLink as Link } from 'react-router-dom'
 import Fade from 'react-reveal/Fade';
+import { Grid, Cell } from 'baseui/layout-grid';
 import Scrollbar from 'react-custom-scrollbars'
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Paragraph1 } from "baseui/typography"
@@ -10,8 +11,9 @@ import { Paragraph1 } from "baseui/typography"
 // components
 //import HomeSliderSlick from '../Components/Home/HomeSliderSlick'
 import { LogoVertical } from '../Svg/Logo'
-//import Header from '../Components/Header/Header'
+import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer'
+import PageContainer from '../Components/PageContainer'
 
 // assets
 import homeImg from '../assets/images/home/home-image.jpg'
@@ -20,57 +22,107 @@ import homeImgThumb from '../assets/images/home/home-image_thumb.jpg'
 
 function Home2() {
   return (
-    <Block
-      display="flex"
-      //flexWrap="wrap"
-      justifyContent="flex-end"
-    //justifyContent="center"
-    //height="100vh"
-    /*display="flex"
-    justifyContent="flex-end"
-    flexWrap="wrap"*/
-    >
+    <>
+      <Header onlyMobile />
       <Block
-        padding="0 15px"
-        //width="85%" 
-        width="100%"
         display="flex"
-        justifyContent="flex-end"
-        alignItems="center"
+        height="100vh"
       >
-        <Scrollbar
-          autoHide
-          renderThumbVertical={({ style, ...props }) =>
-            <div {...props} style={{ ...style, display: "none" }} />
-          }
-          style={{ width: '100%', height: '100%' }}
+        <Block
+          padding="0 15px"
+          //width="85%" 
+          width="100%"
+        /*display="flex"
+        justifyContent="flex-end"
+        alignItems="center"*/
         >
-          <LazyLoadImage
-            wrapperClassName="home-banner__wrapper"
-            src={homeImg}
-            alt="home"
-            effect="blur"
-            placeholderSrc={homeImgThumb}
-          />
-          <Paragraph1 className="home-text" $style={{ fontWeight: 300 }}>
-            <span>Central Marginal</span> es un podcast sobre arte en el caribe conducido por la comunicadora dominicana Silvana Luna. El descubrimiento del archivo de arte del <span>colectivo sincrético</span> y los documentos de la <span>escuela flotante</span> forman la primera serie de episodios. A través de conversaciones con fuentes primarias y especialistas en el archivo, imaginamos futuros posibles para la educación artística en Repúblia Dominicana y el Caribe. <span>Central Marginal</span> es una coproducción con Suspicious Package.
+          <Scrollbar
+            autoHide
+            renderThumbVertical={({ style, ...props }) =>
+              <div {...props} style={{ ...style, display: "none" }} />
+            }
+            style={{ width: '100%', height: '100%' }}
+          >
+            <LazyLoadImage
+              wrapperClassName="home-banner__wrapper"
+              src={homeImg}
+              alt="home"
+              effect="blur"
+              placeholderSrc={homeImgThumb}
+            />
+            <Paragraph1 className="home-text" $style={{ fontWeight: 300 }}>
+              <span>Central Marginal</span> es un podcast sobre arte en el caribe conducido por la comunicadora dominicana Silvana Luna. El descubrimiento del archivo de arte del <span>colectivo sincrético</span> y los documentos de la <span>escuela flotante</span> forman la primera serie de episodios. A través de conversaciones con fuentes primarias y especialistas en el archivo, imaginamos futuros posibles para la educación artística en Repúblia Dominicana y el Caribe. <span>Central Marginal</span> es una coproducción con Suspicious Package.
           </Paragraph1>
 
-          <Footer />
+            <Footer />
 
-        </Scrollbar>
+          </Scrollbar>
+        </Block>
+
+        <Block
+          paddingLeft="5px"
+          paddingRight="20px"
+          maxWidth="15%"
+          //position="fixed"
+          display={["none", "none", "block"]}
+        >
+          <Link to="/">
+            <Fade right>
+              <LogoVertical />
+            </Fade>
+          </Link>
+        </Block>
       </Block>
-
-      <Block
-        paddingLeft="5px"
-        paddingRight="20px"
+      {/*<Block
+        display="flex"
+        //flexWrap="wrap"
+        justifyContent="flex-end"
+      //justifyContent="center"
+      //height="100vh"
       >
-        <Link to="/">
-          <Fade right>
-            <LogoVertical />
-          </Fade>
-        </Link>
-      </Block>
+        <Block
+          padding="0 15px"
+          //width="85%" 
+          width="100%"
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          <Scrollbar
+            autoHide
+            renderThumbVertical={({ style, ...props }) =>
+              <div {...props} style={{ ...style, display: "none" }} />
+            }
+            style={{ width: '100%', height: '100%' }}
+          >
+            <LazyLoadImage
+              wrapperClassName="home-banner__wrapper"
+              src={homeImg}
+              alt="home"
+              effect="blur"
+              placeholderSrc={homeImgThumb}
+            />
+            <Paragraph1 className="home-text" $style={{ fontWeight: 300 }}>
+              <span>Central Marginal</span> es un podcast sobre arte en el caribe conducido por la comunicadora dominicana Silvana Luna. El descubrimiento del archivo de arte del <span>colectivo sincrético</span> y los documentos de la <span>escuela flotante</span> forman la primera serie de episodios. A través de conversaciones con fuentes primarias y especialistas en el archivo, imaginamos futuros posibles para la educación artística en Repúblia Dominicana y el Caribe. <span>Central Marginal</span> es una coproducción con Suspicious Package.
+          </Paragraph1>
+
+            <Footer />
+
+          </Scrollbar>
+        </Block>
+
+        <Block
+          paddingLeft="5px"
+          paddingRight="20px"
+          //maxWidth="15%"
+          display={["none", "none", "block"]}
+        >
+          <Link to="/">
+            <Fade right>
+              <LogoVertical />
+            </Fade>
+          </Link>
+          </Block>*/}
 
 
       {/*<Block
@@ -91,7 +143,7 @@ function Home2() {
 
 
       {/*<Footer />*/}
-    </Block>
+    </>
   )
 }
 
